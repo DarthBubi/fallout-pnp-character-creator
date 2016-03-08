a7aa673e8c0c990fdbed9d907f2867e3f76f27cd
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2016-03-08 17:09:38 +0100

    Add dummy actions to mainView

--- character-creator/main.py
+++ character-creator/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 import mainView
 
 
@@ -8,11 +8,24 @@ class CharacterCreator(QtWidgets.QMainWindow, mainView.Ui_MainWindow):
     def __init__(self, parent=None):
         super(CharacterCreator, self).__init__(parent)
         self.setupUi(self)
-        self.quit()
+        self.quitAction.triggered.connect(self.quit)
+        self.aboutAction.triggered.connect(self.about)
+        self.openCharacterAction.triggered.connect(self.file_open)
+
+    def file_open(self):
+        name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
 
     def quit(self):
-        self.actionQuit.setShortcut("Ctrl+Q")
-        self.actionQuit.triggered.connect(QtCore.QCoreApplication.instance().quit)
+        choice = QtWidgets.QMessageBox.question(self, 'Quit Application', "Do you want to quit the application?",
+                                                QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+        if choice == QtWidgets.QMessageBox.Yes:
+            sys.exit()
+        else:
+            pass
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, 'About Character Creator',
+                                    "This version 0.1.0 of the Fallout PnP Character Creator")
 
 
 def main():
