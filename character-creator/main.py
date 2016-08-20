a53a105f58b801705900d370cbcd40a186e7acc8
Johannes Hackbarth <joha@informatik.uni-bremen.de>
2016-08-20 12:23:41 +0200

    Add new character dialogue Skill tagging does not work yet. There might be some minor bugs during the creation.

--- character-creator/main.py
+++ character-creator/main.py
@@ -6,6 +6,7 @@ import pickle
 import character
 import main_view
 import about_dialogue
+import new_character_dialogue
 
 __author__ = "Johannes Hackbarth"
 
@@ -29,7 +30,7 @@ class CharacterCreator(QtWidgets.QMainWindow, main_view.Ui_MainWindow):
         self.quitAction.triggered.connect(self.close)
         self.aboutAction.triggered.connect(self.about)
         self.openCharacterAction.triggered.connect(self.file_open)
-        self.newCharacterAction.triggered.connect(self.new_character_dummy)
+        self.newCharacterAction.triggered.connect(self.new_character)
         self.saveCharacterAction.triggered.connect(self.save_characters_to_file)
         self.importCharacterAction.triggered.connect(self.import_character)
         self.exportCharacterAction.triggered.connect(self.export_character)
@@ -38,9 +39,10 @@ class CharacterCreator(QtWidgets.QMainWindow, main_view.Ui_MainWindow):
         self.characterListWidget.currentItemChanged.connect(self.show_skills)
         self.list_characters()
 
-    # TODO: Implement character generation dialogue
     def new_character(self):
-        pass
+        new_character = NewCharacterDialogue()
+        if new_character.exec_():
+            self.add_character_to_dict(new_character.get_character())
 
     def list_characters(self):
         for name in self.character_dict:
@@ -154,6 +156,302 @@ class CharacterCreator(QtWidgets.QMainWindow, main_view.Ui_MainWindow):
     def about():
         dialogue = AboutDialogue()
         dialogue.exec_()
+
+    def get_character_dict(self):
+        return self.character_dict
+
+    def add_character_to_dict(self, char):
+        char_item = QtWidgets.QListWidgetItem(char.name)
+        self.character_dict.update({char_item.text(): char})
+        self.characterListWidget.addItem(char_item)
+
+
+# TODO: Add traits and items to character generation
+class NewCharacterDialogue(QtWidgets.QDialog, new_character_dialogue.Ui_Dialog):
+
+    def __init__(self, parent=None):
+        super(NewCharacterDialogue, self).__init__(parent)
+        self.setupUi(self)
+
+        self.available_skill_points = 5  # magic number
+        self.tagged_skills = ([], 0)
+
+        self.sexPicker.clear()
+        self.sexPicker.addItems(["male", "female"])
+
+        race_list = sorted(["Human", "Dog", "Super Mutant", "Half Mutant", "Ghoul", "Deathclaw", "Robot"])
+        self.racePicker.clear()
+        self.racePicker.addItems(race_list)
+        self.racePicker.currentIndexChanged.connect(self.handle_race_change)
+        self.handle_race_change()
+        self.availablePointsBox.setText(str(self.available_skill_points))
+
+        self.strengthSpinBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.perceptionBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.enduranceBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.charismaBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.intelligenceBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.agilityBox.valueChanged.connect(self.handle_attribute_value_change)
+        self.luckBox.valueChanged.connect(self.handle_attribute_value_change)
+
+        self.smallGunsTag.clicked.connect(self.handle_strength_tag)
+        self.bigGunsTag.clicked.connect(self.handle_skill_tags)
+        self.energyWeaponsTag.clicked.connect(self.handle_skill_tags)
+        self.unarmedTag.clicked.connect(self.handle_skill_tags)
+        self.meleeWeaponsTag.clicked.connect(self.handle_skill_tags)
+        self.throwingTag.clicked.connect(self.handle_skill_tags)
+        self.explosivesTag.clicked.connect(self.handle_skill_tags)
+        self.doctorTag.clicked.connect(self.handle_skill_tags)
+        self.sneakTag.clicked.connect(self.handle_skill_tags)
+        self.lockpickTag.clicked.connect(self.handle_skill_tags)
+        self.trapsTag.clicked.connect(self.handle_skill_tags)
+        self.scienceTag.clicked.connect(self.handle_skill_tags)
+        self.repairTag.clicked.connect(self.handle_skill_tags)
+        self.pilotTag.clicked.connect(self.handle_skill_tags)
+        self.speechTag.clicked.connect(self.handle_skill_tags)
+        self.barterTag.clicked.connect(self.handle_skill_tags)
+        self.gamblingTag.clicked.connect(self.handle_skill_tags)
+        self.survivalTag.clicked.connect(self.handle_skill_tags)
+
+        self.nextButton = QtWidgets.QPushButton("Next")
+        self.nextButton.clicked.connect(self.next_page)
+        self.backButton = QtWidgets.QPushButton("Back")
+        self.backButton.clicked.connect(self.previous_page)
+        self.buttonBox.addButton(self.backButton, QtWidgets.QDialogButtonBox.ActionRole)
+        self.buttonBox.addButton(self.nextButton, QtWidgets.QDialogButtonBox.ActionRole)
+        self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).clicked.connect(self.finish_character_creation)
+        self.buttonBox.button(QtWidgets.QDialogButtonBox.Cancel).clicked.connect(self.close)
+        self.stackedWidget.setCurrentIndex(0)
+
+    def handle_race_change(self):
+        self.selected_race()
+        self.set_attribute_limits()
+        # self.strengthSpinBox.blockSignals(True)
+        # self.perceptionBox.blockSignals(True)
+        # self.enduranceBox.blockSignals(True)
+        # self.charismaBox.blockSignals(True)
+        # self.intelligenceBox.blockSignals(True)
+        # self.agilityBox.blockSignals(True)
+        # self.luckBox.blockSignals(True)
+        self.set_default_attribute_values()
+        self.available_skill_points = 5
+        self.set_skill_values()
+
+    def handle_attribute_value_change(self):
+        if self.available_skill_points > 0:
+            if self.character.strength != self.strengthSpinBox.value():
+                if self.character.strength > self.strengthSpinBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.strength = self.strengthSpinBox.value()
+
+            elif self.character.perception != self.perceptionBox.value():
+                if self.character.perception > self.perceptionBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.perception = self.perceptionBox.value()
+
+            elif self.character.endurance != self.enduranceBox.value():
+                if self.character.endurance > self.enduranceBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.endurance = self.enduranceBox.value()
+
+            elif self.character.charisma != self.charismaBox.value():
+                if self.character.charisma > self.charismaBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.charisma = self.charismaBox.value()
+
+            elif self.character.intelligence != self.intelligenceBox.value():
+                if self.character.intelligence > self.intelligenceBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.intelligence = self.intelligenceBox.value()
+
+            elif self.character.agility != self.agilityBox.value():
+                if self.character.agility > self.agilityBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.agility = self.agilityBox.value()
+
+            elif self.character.luck != self.luckBox.value():
+                if self.character.luck > self.luckBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.available_skill_points -= 1
+                self.character.luck = self.luckBox.value()
+
+            self.availablePointsBox.setText(str(self.available_skill_points))
+
+        elif self.available_skill_points == 0:
+            if self.character.strength != self.strengthSpinBox.value():
+                if self.character.strength > self.strengthSpinBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.strengthSpinBox.setValue(self.character.strength)
+                self.character.strength = self.strengthSpinBox.value()
+
+            elif self.character.perception != self.perceptionBox.value():
+                if self.character.perception > self.perceptionBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.perceptionBox.setValue(self.character.perception)
+                self.character.perception = self.perceptionBox.value()
+
+            elif self.character.endurance != self.enduranceBox.value():
+                if self.character.endurance > self.enduranceBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.enduranceBox.setValue(self.character.endurance)
+                self.character.endurance = self.enduranceBox.value()
+
+            elif self.character.charisma != self.charismaBox.value():
+                if self.character.charisma > self.charismaBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.charismaBox.setValue(self.character.charisma)
+                self.character.charisma = self.charismaBox.value()
+
+            elif self.character.intelligence != self.intelligenceBox.value():
+                if self.character.intelligence > self.intelligenceBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.intelligenceBox.setValue(self.character.intelligence)
+                self.character.intelligence = self.intelligenceBox.value()
+
+            elif self.character.agility != self.agilityBox.value():
+                if self.character.agility > self.agilityBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.agilityBox.setValue(self.character.agility)
+                self.character.agility = self.agilityBox.value()
+
+            elif self.character.luck != self.luckBox.value():
+                if self.character.luck > self.luckBox.value():
+                    self.available_skill_points += 1
+                else:
+                    self.luckBox.setValue(self.character.luck)
+                self.character.luck = self.luckBox.value()
+
+            self.availablePointsBox.setText(str(self.available_skill_points))
+
+    # test method
+    def handle_strength_tag(self):
+        if self.smallGunsTag.isChecked() and self.tagged_skills[1] < 3:
+            self.character.small_guns += 20
+            self.smallGunsBox.setValue(self.character.small_guns)
+        elif self.tagged_skills[0].__contains__("small_guns"):
+            self.character.small_guns -= 20
+            self.smallGunsBox.setValue(self.character.small_guns)
+
+    # TODO: Handle skill tagging
+    def handle_skill_tags(self):
+        pass
+
+    def next_page(self):
+        if self.stackedWidget.currentIndex() < self.stackedWidget.count():
+            self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex()+1)
+            if self.stackedWidget.currentIndex() == 2:
+                self.set_skill_values()
+
+    def previous_page(self):
+        if self.stackedWidget.currentIndex() > 0:
+            self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex()-1)
+
+    def selected_race(self):
+        if self.racePicker.currentText() == "Deathclaw":
+            self.character = character.DeathclawCharacter()
+        elif self.racePicker.currentText() == "Dog":
+            self.character = character.DogCharacter()
+        elif self.racePicker.currentText() == "Ghoul":
+            self.character = character.GhoulCharacter()
+        elif self.racePicker.currentText() == "Half Mutant":
+            self.character = character.HalfMutantCharacter()
+        elif self.racePicker.currentText() == "Human":
+            self.character = character.HumanCharacter()
+        elif self.racePicker.currentText() == "Robot":
+            self.character = character.RobotCharacter()
+        elif self.racePicker.currentText() == "Super Mutant":
+            self.character = character.SuperMutantCharacter()
+
+    def set_default_attribute_values(self):
+        self.strengthSpinBox.blockSignals(True)
+        self.strengthSpinBox.setValue(self.character.strength)
+        self.strengthSpinBox.blockSignals(False)
+        self.perceptionBox.blockSignals(True)
+        self.perceptionBox.setValue(self.character.perception)
+        self.perceptionBox.blockSignals(False)
+        self.enduranceBox.blockSignals(True)
+        self.enduranceBox.setValue(self.character.endurance)
+        self.enduranceBox.blockSignals(False)
+        self.charismaBox.blockSignals(True)
+        self.charismaBox.setValue(self.character.charisma)
+        self.charismaBox.blockSignals(False)
+        self.intelligenceBox.blockSignals(True)
+        self.intelligenceBox.setValue(self.character.intelligence)
+        self.intelligenceBox.blockSignals(False)
+        self.agilityBox.blockSignals(True)
+        self.agilityBox.setValue(self.character.agility)
+        self.agilityBox.blockSignals(False)
+        self.luckBox.blockSignals(True)
+        self.luckBox.setValue(self.character.luck)
+        self.luckBox.blockSignals(False)
+
+    def set_attribute_limits(self):
+        self.strengthSpinBox.setMinimum(self.character.MIN_STRENGTH)
+        self.strengthSpinBox.setMaximum(self.character.MAX_STRENGTH)
+        self.perceptionBox.setMinimum(self.character.MIN_PERCEPTION)
+        self.perceptionBox.setMaximum(self.character.MAX_PERCEPTION)
+        self.enduranceBox.setMinimum(self.character.MIN_ENDURANCE)
+        self.enduranceBox.setMaximum(self.character.MAX_ENDURANCE)
+        self.charismaBox.setMinimum(self.character.MIN_CHARISMA)
+        self.charismaBox.setMaximum( self.character.MAX_CHARISMA)
+        self.intelligenceBox.setMinimum(self.character.MIN_INTELLIGENCE)
+        self.intelligenceBox.setMaximum(self.character.MAX_INTELLIGENCE)
+        self.agilityBox.setMinimum(self.character.MIN_AGILITY)
+        self.agilityBox.setMaximum(self.character.MAX_AGILITY)
+        self.luckBox.setMinimum(self.character.MIN_LUCK)
+        self.luckBox.setMaximum(self.character.MAX_LUCK)
+
+    def set_skill_values(self):
+        self.character.calculate_base_skills()
+
+        self.smallGunsBox.setValue(self.character.small_guns)
+        self.bigGunsBox.setValue(self.character.big_guns)
+        self.energyWeaponsBox.setValue(self.character.energy_weapons)
+        self.unarmedBox.setValue(self.character.unarmed)
+        self.meleeWeaponsBox.setValue(self.character.melee_weapons)
+        self.throwingBox.setValue(self.character.throwing)
+        self.explosivesBox.setValue(self.character.explosives)
+        self.doctorBox.setValue(self.character.doctor)
+        self.sneakBox.setValue(self.character.sneak)
+        self.lockpickBox.setValue(self.character.lockpick)
+        self.trapsBox.setValue(self.character.traps)
+        self.scienceBox.setValue(self.character.science)
+        self.repairBox.setValue(self.character.repair)
+        self.pilotBox.setValue(self.character.pilot)
+        self.speechBox.setValue(self.character.speech)
+        self.barterBox.setValue(self.character.barter)
+        self.gamblingBox.setValue(self.character.gambling)
+        self.survivalBox.setValue(self.character.survival)
+
+    # TODO: Check if all fields are filled with valid values
+    def finish_character_creation(self):
+        self.character.name = self.nameField.text()
+        self.character.age = self.ageField.text()
+        self.character.sex = self.sexPicker.currentText()
+        self.character.calculate_base_skills()
+        self.accept()
+
+    def get_character(self):
+        return self.character
 
 
 class AboutDialogue(QtWidgets.QDialog, about_dialogue.Ui_Dialog):
